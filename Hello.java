2f34c364a2057992ee513b52fe048388f8dc639d
Tushar <mtl12293@gmail.com>
2025-11-19 16:49:41 +0530

    added new changes in feature 1branch

--- Hello.java
+++ Hello.java
@@ -3,9 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("hello java ");
-        System.out.println("this is new changes microservice 1 java ");
+        System.out.println("this is new changes feature 1 branch ");
 
-        System.out.println("hello microservices 2  java ");
 
     }
 }
