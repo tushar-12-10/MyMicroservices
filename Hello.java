157a1adbc340f5fd0f03269735f5588bc660ebfd
Tushar <mtl12293@gmail.com>
2025-11-19 15:56:11 +0530

    added microservice 2 in hello

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("hello java ");
+        System.out.println("hello microservices 2  java ");
+
     }
 }
